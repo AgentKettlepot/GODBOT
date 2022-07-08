98d51901f991bc8317b0bbad3926c468f8da90c6
Keith Yao <keithyao1012@gmail.com>
2022-07-07 21:23:18 -0400

    Added function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import Info
 import re
 from datetime import datetime 
+import csv
 client = discord.Client()
 
 @client.event
@@ -18,8 +19,17 @@ async def on_message(message):
     
     if message.content.startswith('/setdailymax'):
         time = re.findall('[0-9]+', message.content)
-        print(time)
         now = datetime.now()
-        await message.channel.send(f'{time} minute timer set')
+        end_time = now + time
+        with open('logs.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow([message.author, now, time, end_time])
+ 
+        await message.channel.send(f'{time[0]} minute timer set')
 
+
+#add a function that is async def on_repeat (idk)
+#keeps reading the .csv file, looks for anyone has messaged:
+#if message.time is within the endtime column for message.author: send message
+#remove elements where the end_time is already over
 client.run(Info.TOKEN)
