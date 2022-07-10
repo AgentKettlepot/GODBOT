951aef46b75176c0daf4bcade21f27f843460ff7
Keith Yao <keithyao1012@gmail.com>
2022-07-10 19:32:36 -0400

    Added /cancel function

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
+
     updatedlist=[] #this part reads the .csv file and removes the users where their endtimes are over
     with open("logs.csv",newline="") as f:
       reader=csv.reader(f)
@@ -22,9 +23,26 @@ async def on_message(message):
         try:
             if str(row[3])>str(datetime.now()):
                 updatedlist.append(row)
+            else:
+                await message.channel.send(f'@{row[0]} has finished their focus session!')
         except:
             pass
       UpdateFile(updatedlist)
+
+    if message.content.startswith('/cancel'):#this part cancels the user's focus mode
+        with open('logs.csv', 'r') as f:
+            reader = csv.reader(f)
+            current_list = []
+            for line in reader:
+                current_list.append(line)
+            if str(message.author) not in str([i[0] for i in current_list]): #if the user who uses /cancel is not in focus mode
+                await message.channel.send('Currently not in focus mode')
+            else:
+                updatedlist = [i for i in current_list if str(i[0]) != str(message.author)]
+                UpdateFile(updatedlist)
+                await message.channel.send(f'{message.author} has been removed from focus mode!')
+
+
 
     with open('logs.csv', 'r') as f: #this part reads the .csv file when someone sends a message
         reader = csv.reader(f)
@@ -34,9 +52,7 @@ async def on_message(message):
                     await message.channel.send(f'{message.author} should NOT be on Discord!!!!')
                     break
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!'+ str(message.author))
-    
+
     if message.content.startswith('/setdailymax'):
         time = re.findall('[0-9]+', message.content)
         now = datetime.now()
@@ -47,21 +63,22 @@ async def on_message(message):
  
         await message.channel.send(f'{time[0]} minute timer set')
 
+
+
+
+
+
     if message.content.startswith('/onfocus'):# displays the users currently on focus mode
-        with open('logs.csv', 'r') as f: #this part reads the .csv file when someone sends a message
+        with open('logs.csv', 'r') as f:
             reader = csv.reader(f)
             
             for line in reader:
                 await message.channel.send(line)
 
-
-
 def UpdateFile(updatedlist): #used to update the new log.csv file everytime a endtime is reached
     with open("logs.csv","w",newline="") as f:
         Writer=csv.writer(f)
-        #Writer.writerow(['user', 'begin_time', 'duration', 'endtime'])
         Writer.writerows(updatedlist)
-        print("File has been updated")
 
 
 bot.run(Info.TOKEN)
